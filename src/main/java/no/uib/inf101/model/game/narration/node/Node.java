33ce2799876b28ebf37130473941d3c903a9fd9b
Sunniva <sunnivanordbjerga@gmail.com>
2026-04-27 20:40:49 +0200

    implemented Node interface

---
+++ src/main/java/no/uib/inf101/model/game/narration/node/Node.java
@@ -0,0 +1,42 @@
+package no.uib.inf101.model.game.narration.node;
+
+import no.uib.inf101.model.game.Game;
+import no.uib.inf101.model.game.narration.Choice;
+
+import java.util.List;
+
+/**
+ * Defines functionality for game event nodes
+ * like a combat encounter or story progression.
+ */
+public interface Node {
+
+    /**
+     * Returns the text belonging to this {@link Node}
+     *
+     * @return the node text
+     */
+    String getText();
+
+    /**
+     * Returns the {@link Choice}s available from this {@link Node}.
+     *
+     * @return the list of the choices belonging to this node
+     */
+    List<Choice> getChoices();
+
+    /**
+     * Adds an option to the list of {@link Choice}s.
+     *
+     * @param choice the choice to add
+     */
+    void addChoice(Choice choice);
+
+    /**
+     * Calls on the {@link Game} to execute any logic belonging to this {@link Node},
+     * like starting a combat encounter or performing a skill check.
+     *
+     * @param game the game model to use
+     */
+    void onEnter(Game game);
+}
