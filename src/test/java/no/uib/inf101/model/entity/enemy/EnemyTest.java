dd2feab8ba950a21c88e0b2ee08d71b90460ea88
Sunniva <sunnivanordbjerga@gmail.com>
2026-04-28 16:12:48 +0200

    EnemyTest

---
+++ src/test/java/no/uib/inf101/model/entity/enemy/EnemyTest.java
@@ -0,0 +1,34 @@
+package no.uib.inf101.model.entity.enemy;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Tests {@link Enemy}.
+ */
+class EnemyTest {
+    private Enemy enemy;
+
+    @BeforeEach
+    void setup(){
+        this.enemy = new Enemy("Enemy", 10, 2, 8);
+    }
+    @Test
+    void throwsOnNegativeMaxHp(){
+        assertThrows(IllegalArgumentException.class, () -> new Enemy("Illegal",-3,2,5));
+    }
+
+    @Test
+    void throwsOnBlankDisplayName(){
+        assertThrows(IllegalArgumentException.class, () -> new Enemy(" ",3,2,5));
+    }
+
+    @Test
+    void takeDamage_stopsAtHpZero(){
+        enemy.takeDamage(enemy.getMaxHp());
+        assertEquals(0, enemy.getHp());
+    }
+
+}
