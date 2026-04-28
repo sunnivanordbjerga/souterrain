a897d2008e74709d120e0e774553cc24c25f859e
Sunniva <sunnivanordbjerga@gmail.com>
2026-04-28 23:53:06 +0200

    added gameOver to ControllableGame

--- src/main/java/no/uib/inf101/controller/ControllableGame.java
+++ src/main/java/no/uib/inf101/controller/ControllableGame.java
@@ -6,4 +6,13 @@ import no.uib.inf101.model.game.Game;
  * Defines {@link Game} methods usable by the {@link GameController}
  */
 public interface ControllableGame {
+
+    /**
+     * Returns whether the game is over.
+     *
+     * @return {@code true} if the game is over
+     */
+    boolean gameOver();
+
+    //TODO: Implement ControllableGame
 }
