9d4f51b286145a4cda4fd77a2d82096a6eea09fa
Sunniva <sunnivanordbjerga@gmail.com>
2026-04-28 16:12:32 +0200

    implemented AttackResult

---
+++ src/main/java/no/uib/inf101/model/game/AttackResult.java
@@ -0,0 +1,9 @@
+package no.uib.inf101.model.game;
+
+/**
+ * Contains details about an attack outcome.
+ *
+ * @param damageDealt the damage dealt to the target
+ */
+public record AttackResult(int damageDealt) {
+}
