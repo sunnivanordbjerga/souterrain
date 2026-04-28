2fea3f8c48cf44fad4abb439312db1db60289842
Sunniva <sunnivanordbjerga@gmail.com>
2026-04-28 23:47:18 +0200

    WolfsTooth

---
+++ src/main/java/no/uib/inf101/model/loot/equipable/WolfsTooth.java
@@ -0,0 +1,28 @@
+package no.uib.inf101.model.loot.equipable;
+
+import no.uib.inf101.model.entity.player.Player;
+
+/**
+ * Represents the sword dropped by the dire-wolf, providing a
+ * damage buff to the {@link Player} while equipped.
+ */
+public class WolfsTooth implements Equipable {
+    private static final String DISPLAY_NAME = "Wolf's Tooth";
+    private static final int DAMAGE_BONUS = 2;
+
+
+    @Override
+    public void equip(Player player) {
+        player.modifyAttackRange(DAMAGE_BONUS);
+    }
+
+    @Override
+    public void unequip(Player player) {
+        player.modifyAttackRange(-DAMAGE_BONUS);
+    }
+
+    @Override
+    public String getDisplayName() {
+        return DISPLAY_NAME;
+    }
+}
