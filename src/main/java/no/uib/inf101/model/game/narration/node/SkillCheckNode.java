34a6edc9ad62f88ad8ddbfaea90cd85f4c117e77
Sunniva <sunnivanordbjerga@gmail.com>
2026-04-28 17:12:04 +0200

    simplified SkillCheckNode

--- src/main/java/no/uib/inf101/model/game/narration/node/SkillCheckNode.java
+++ src/main/java/no/uib/inf101/model/game/narration/node/SkillCheckNode.java
@@ -12,11 +12,9 @@ import java.util.Objects;
  * and 100 (guaranteed success).
  */
 public class SkillCheckNode extends AbstractNode {
-    private static final int MAX_SUCCESS_CHANCE = 100;
+    private static final int MAX_SUCCESS_RESULT = 100;
 
     private final int successChance;
-    private final String successText;
-    private final String failureText;
     private final Node successNode;
     private final Node failureNode;
 
@@ -24,36 +22,24 @@ public class SkillCheckNode extends AbstractNode {
      * Creates a {@link SkillCheckNode}.
      *
      * @param baseText      the text to show before both outcomes
-     * @param successText   the text to show on success; cannot be blank
-     * @param failureText   the text to show on failure; cannot be blank
      * @param successNode   the {@link Node} to proceed to on success
      * @param failureNode   the {@link Node} to proceed to on failure
      * @param successChance the chance of success (0-100)
      * @throws NullPointerException     if any arguments are null
      * @throws IllegalArgumentException if successChance is not between 0-100
-     * @throws IllegalArgumentException if any text arguments are blank
+     * @throws IllegalArgumentException if baseText is blank
      */
     public SkillCheckNode(
             String baseText,
-            String successText,
-            String failureText,
             Node successNode,
             Node failureNode,
             int successChance) {
 
         super(baseText);
-        this.successText = Objects.requireNonNull(successText, "SuccessText cannot be null");
-        this.failureText = Objects.requireNonNull(failureText, "FailureText cannot be null");
         this.successNode = Objects.requireNonNull(successNode, "SuccessNode cannot be null");
         this.failureNode = Objects.requireNonNull(failureNode, "FailureNode cannot be null");
         this.successChance = successChance;
 
-        if (successText.isBlank()) {
-            throw new IllegalArgumentException("SuccessText cannot be blank");
-        }
-        if (failureText.isBlank()) {
-            throw new IllegalArgumentException("FailureText cannot be blank");
-        }
         if (successChance < 0 || successChance > 100) {
             throw new IllegalArgumentException("SuccessChance must be between 0 and 100");
         }
@@ -63,14 +49,8 @@ public class SkillCheckNode extends AbstractNode {
     public void onEnter(Game game) {
         super.onEnter(game);
 
-        boolean success = game.getRandom().nextInt(MAX_SUCCESS_CHANCE) < successChance;
+        boolean success = game.getRandom().nextInt(MAX_SUCCESS_RESULT) <= successChance;
 
-        if (success) {
-            game.log(successText);
-            game.setCurrentNode(successNode);
-        } else {
-            game.log(failureText);
-            game.setCurrentNode(failureNode);
-        }
+        game.setCurrentNode(success ? successNode : failureNode);
     }
 }
