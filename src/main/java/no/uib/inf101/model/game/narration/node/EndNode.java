56448fba705a2f0b32bc3786b8ec3fa2559d640e
Sunniva <sunnivanordbjerga@gmail.com>
2026-04-28 23:55:53 +0200

    implemented EndNode

---
+++ src/main/java/no/uib/inf101/model/game/narration/node/EndNode.java
@@ -0,0 +1,18 @@
+package no.uib.inf101.model.game.narration.node;
+
+import no.uib.inf101.model.game.Game;
+
+/**
+ * A {@link Node} that ends the game. Used on game over or game win events.
+ */
+public class EndNode extends AbstractNode {
+    public EndNode(String text) {
+        super(text);
+    }
+
+    @Override
+    public void onEnter(Game game) {
+        super.onEnter(game);
+        game.setGameOver();
+    }
+}
