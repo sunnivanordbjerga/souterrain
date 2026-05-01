ba950e550395cd82d9188b6fea70fd4053bc5002
Sunniva <sunnivanordbjerga@gmail.com>
2026-05-01 13:05:22 +0200

    tested player attack() and modifyAttackRange()

--- src/test/java/no/uib/inf101/model/entity/player/PlayerTest.java
+++ src/test/java/no/uib/inf101/model/entity/player/PlayerTest.java
@@ -1,9 +1,14 @@
 package no.uib.inf101.model.entity.player;
 
+import no.uib.inf101.model.entity.enemy.Enemy;
+import no.uib.inf101.model.game.AttackResult;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Tests {@link Player}.
@@ -29,56 +34,101 @@ class PlayerTest {
 
     @Test
     void takeDamage_decreasesByDefenceRating() {
+        int before = player.getHp();
         player.takeDamage(10);
-        assertEquals(player.getMaxHp() - 10 + player.getDefense(), player.getHp());
+        int after = before - player.getHp();
+
+        int expected = Math.max(1, 10 - player.getDefense());
+
+        assertEquals(expected, after);
+    }
+
+    @Test
+    void attack_reducesTargetHp() {
+        Random random = new Random(1);
+        Enemy grunt = new Enemy("Grunt", 10, 2, 5);
+
+        int before = grunt.getHp();
+
+        AttackResult result = player.attack(grunt, random);
+
+        assertEquals(before - result.damageDealt(), grunt.getHp());
+    }
+
+    @Test
+    void attack_returnsDamageWithinAttackRange() {
+        Random random = new Random(1);
+        Enemy grunt = new Enemy("Grunt", 10, 2, 5);
+
+        int result = player.attack(grunt, random).damageDealt();
+        assertTrue(result >= 4 && result <= 7);
     }
 
     @Test
     void heal_capsAtMaxHp() {
+        assertEquals(player.getMaxHp(), player.getHp());
         player.heal(1);
         assertEquals(player.getMaxHp(), player.getHp());
     }
 
     @Test
     void modifyDefenseBonus_increasesDefence() {
+        int before = player.getDefense();
         player.modifyEquipmentBonus(+5);
-        player.takeDamage(10);
-        assertEquals(player.getMaxHp() - 1, player.getHp());
+        assertEquals(before + 5, player.getDefense());
     }
 
     @Test
     void modifyDefenseBonus_decreasesDefence() {
+        int before = player.getDefense();
+
         player.modifyEquipmentBonus(+5);
-        assertEquals(10, player.getDefense());
-
         player.modifyEquipmentBonus(-3);
-        assertEquals(7, player.getDefense());
 
-        player.takeDamage(7);
-        assertEquals(player.getMaxHp() - 1, player.getHp());
-    }
-
-    //TODO: test modifyAttackRange
-
-    @Test
-    void modifyAttackRange_capsAtZero(){
-
+        assertEquals(before + 5 - 3, player.getDefense());
     }
 
     @Test
     void modifyAttackRange_increasesDamage() {
+        Enemy grunt = new Enemy("Grunt", 10, 2, 5);
 
+        Random random = new Random(1);
+        int before = player.attack(grunt, random).damageDealt();
+
+        player.modifyAttackRange(2);
+
+        random = new Random(1);
+        int after = player.attack(grunt, random).damageDealt();
+        assertEquals(before + 2, after);
     }
 
     @Test
     void modifyAttackRange_decreasesDamage() {
+        Enemy grunt = new Enemy("Grunt", 10, 2, 5);
 
+        Random random = new Random(1);
+        int before = player.attack(grunt, random).damageDealt();
+
+        player.modifyAttackRange(5);
+        player.modifyAttackRange(-2);
+
+        random = new Random(1);
+        int after = player.attack(grunt, random).damageDealt();
+        assertEquals(before + 5 - 2, after);
     }
 
     @Test
-    void modifyAttackRange_capsAtBase(){
+    void modifyAttackRange_capsAtZero() {
+        Enemy grunt = new Enemy("Grunt", 10, 2, 5);
 
+        Random random = new Random(1);
+        int before = player.attack(grunt, random).damageDealt();
+
+        player.modifyAttackRange(-2);
+
+        random = new Random(1);
+        int after = player.attack(grunt, random).damageDealt();
+
+        assertEquals(before, after);
     }
-
-    //TODO: test attack
 }
