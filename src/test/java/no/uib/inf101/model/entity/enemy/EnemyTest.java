bc8c5069b1a0e3f9b1435f9b83913389b2ca7ded
Sunniva <sunnivanordbjerga@gmail.com>
2026-05-01 12:36:34 +0200

    tested Enemy attack()

--- src/test/java/no/uib/inf101/model/entity/enemy/EnemyTest.java
+++ src/test/java/no/uib/inf101/model/entity/enemy/EnemyTest.java
@@ -1,7 +1,11 @@
 package no.uib.inf101.model.entity.enemy;
 
+import no.uib.inf101.model.entity.player.Player;
+import no.uib.inf101.model.game.AttackResult;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -12,18 +16,40 @@ class EnemyTest {
     private Enemy enemy;
 
     @BeforeEach
-    void setup(){
+    void setup() {
         this.enemy = new Enemy("Enemy", 10, 2, 8);
     }
+
     @Test
-    void throwsOnNegativeMaxHp(){
-        assertThrows(IllegalArgumentException.class, () -> new Enemy("Illegal",-3,2,5));
+    void throwsOnNegativeMaxHp() {
+        assertThrows(IllegalArgumentException.class, () -> new Enemy("Illegal", -3, 2, 5));
     }
 
     @Test
-    void throwsOnBlankDisplayName(){
-        assertThrows(IllegalArgumentException.class, () -> new Enemy(" ",3,2,5));
+    void throwsOnBlankDisplayName() {
+        assertThrows(IllegalArgumentException.class, () -> new Enemy(" ", 3, 2, 5));
     }
 
-    //TODO: test attack
+    @Test
+    void attack_reducesTargetHp() {
+        Random random = new Random(1);
+        Enemy grunt = new Enemy("Grunt", 10, 2, 5);
+
+        int before = grunt.getHp();
+
+        AttackResult result = enemy.attack(grunt, random);
+
+        assertEquals(before - result.damageDealt(), grunt.getHp());
+    }
+
+    @Test
+    void attack_returnsDamageWithinAttackRange() {
+        Random random = new Random(1);
+        Player hero = new Player();
+
+        AttackResult result = enemy.attack(hero, random);
+
+        assertTrue(result.damageDealt() >= 2);
+        assertTrue(result.damageDealt() <= 8);
+    }
 }
