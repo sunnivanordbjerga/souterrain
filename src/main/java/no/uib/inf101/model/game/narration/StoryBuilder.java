66919d7f7792d202cc6e68392ed1822539dca3b9
Sunniva <sunnivanordbjerga@gmail.com>
2026-05-03 02:29:15 +0200

    update StoryBuilder

--- src/main/java/no/uib/inf101/model/game/narration/StoryBuilder.java
+++ src/main/java/no/uib/inf101/model/game/narration/StoryBuilder.java
@@ -4,28 +4,27 @@ import no.uib.inf101.model.entity.enemy.Enemy;
 import no.uib.inf101.model.game.narration.node.*;
 
 /**
- * Responsible for initializing and connecting all {@link Node}s, grouping them by {@link Scene}s.
+ * Responsible for initializing and connecting all {@link Node}s and {@link Choice}s
  */
 public class StoryBuilder {
+    private Node gameOver;
 
-    public Node buildStory(){
-        return createStartScene();
-    }
-
-    private Node createStartScene() {
+    /**
+     * Builds and connects all {@link Node}s and {@link Choice}s in the story graph.
+     *
+     * @return the first {@link Node}
+     */
+    public Node buildStory() {
         Node start = new StoryNode("The air is stale and uninviting");
+        Node lookAroundStartRoom = createLookAroundStartRoom();
+        this.gameOver = new EndNode("The light leaves your eyes");
 
-        Node lookAround = createLookAroundHub();
-
-        Node proceed = new StoryNode("You move on.");
-
-        start.addChoice(new Choice("Proceed through the door", proceed));
-        start.addChoice(new Choice("Look around", lookAround));
-
+        start.addChoice(new Choice("Proceed through the door", proceedTo(gameOver)));//TODO: Set next room
+        start.addChoice(new Choice("Look around", lookAroundStartRoom));
         return start;
     }
 
-    private Node createLookAroundHub() {
+    private Node createLookAroundStartRoom() {
         Node lookAround = new StoryNode("""
                 The stone walls are damp and overgrown with moss.
                 Drip points in the ceiling form a steady, echoing rhythm,
@@ -36,18 +35,17 @@ public class StoryBuilder {
 
         Node investigateArmor = createArmourBranch(lookAround);
         Node investigateCarvings = createCarvingsBranch(lookAround);
-        Node proceed = new StoryNode("You move on.");
 
         lookAround.addChoice(new Choice("Investigate the armour", investigateArmor));
         lookAround.addChoice(new Choice("Investigate the stone carvings", investigateCarvings));
-        lookAround.addChoice(new Choice("Proceed through the door", proceed));
+        lookAround.addChoice(new Choice("Proceed through the door", proceedTo(gameOver))); //TODO
 
         return lookAround;
     }
 
-    private Node createArmourBranch(Node hub) {
+    private Node createArmourBranch(Node returnNode) {
         Node armorSuccess = new StoryNode("""
-                 It is unmoving, but through the helmet slit,
+                It is unmoving, but through the helmet slit,
                 you swear you see a flicker, as if you're being watched.
                 """);
         Node armorFailure = new StoryNode("""
@@ -57,14 +55,13 @@ public class StoryBuilder {
 
         Enemy undeadGuardian = new Enemy("Undead Guardian", 25, 6, 10);
         Node afterCombat = new StoryNode("The knight crumples to the ground. The armor is hollow.");
-        Node gameOver = new EndNode("The light leaves your eyes");
         Node guardianCombat = new CombatNode(
                 "The armour stirs", undeadGuardian, afterCombat, gameOver
-                );
+        );
 
-        armorSuccess.addChoice(new Choice("Back away slowly", hub));
+        armorSuccess.addChoice(new Choice("Back away slowly", returnNode));
         armorSuccess.addChoice(new Choice("Disturb its slumber", guardianCombat));
-        armorFailure.addChoice(new Choice("Step back", hub));
+        armorFailure.addChoice(new Choice("Step back", returnNode));
         armorFailure.addChoice(new Choice("Take the helmet", guardianCombat));
 
         return new SkillCheckNode(
@@ -74,14 +71,15 @@ public class StoryBuilder {
                 70);
     }
 
-    private Node createCarvingsBranch(Node hub) {
+    private Node createCarvingsBranch(Node returnNode) {
         Node carvingsSuccess = new StoryNode("""
-                You recognise the letters as ancient [some cryptic lore]. Though time has
+                You recognise the letters as an ancient script. Though time has
                 eroded the full message, you can make out a phrase roughly translating to
-                 [HINT]""");
+                ..."on his right-hand side".
+                """);
         Node carvingsFailure = new StoryNode("What little remains legible of the carving make no sense to you.");
 
-        Choice stepBack = new Choice("Step back", hub);
+        Choice stepBack = new Choice("Step back", returnNode);
         carvingsSuccess.addChoice(stepBack);
         carvingsFailure.addChoice(stepBack);
 
@@ -91,5 +89,9 @@ public class StoryBuilder {
                 carvingsFailure,
                 50
         );
+    }
+
+    private Node proceedTo(Node nextNode){
+        return new TransitionNode("You move on.", nextNode);
     }
 }
