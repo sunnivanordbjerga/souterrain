ee35819b9087f07a16a08c73f24fa505f48816c9
sunniva.bjerga <subje9045@uib.no>
2026-05-05 15:10:10 +0200

    simplified ChoiceType names

--- src/main/java/no/uib/inf101/model/game/ChoiceType.java
+++ src/main/java/no/uib/inf101/model/game/ChoiceType.java
@@ -4,7 +4,7 @@ package no.uib.inf101.model.game;
  * High level representation of system event choice outcomes
  */
 public enum ChoiceType {
-    PROGRESS_STORY,
+    NORMAL,
     RESTART,
     QUIT
 }
