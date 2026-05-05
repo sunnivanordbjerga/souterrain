be3701f7717b38436ee3de60936bb4043a366abd
sunniva.bjerga <subje9045@uib.no>
2026-05-05 15:09:50 +0200

    pre-defined choices for EndNode

--- src/main/java/no/uib/inf101/model/game/node/EndNode.java
+++ src/main/java/no/uib/inf101/model/game/node/EndNode.java
@@ -1,18 +1,55 @@
 package no.uib.inf101.model.game.node;
 
-import no.uib.inf101.model.game.Game;
+import no.uib.inf101.model.game.Choice;
+import no.uib.inf101.model.game.ChoiceType;
+
+import java.util.List;
 
 /**
  * A {@link Node} that ends the game. Used on game over or game win events.
+ * Choices are pre-defined.
  */
 public class EndNode extends AbstractNode {
-    public EndNode(String text) {
+    private final EndType endType;
+
+    /**
+     * Defines the ending type.
+     */
+    public enum EndType {
+        GAME_OVER,
+        WIN
+    }
+
+    /**
+     * Creates an {@link EndNode} of the given {@link EndType}.
+     *
+     * @param text    the text corresponding to this node; cannot be blank
+     * @param endType the type of ending this node signifies
+     */
+    public EndNode(String text, EndType endType) {
         super(text);
+        this.endType = endType;
+    }
+
+
+    @Override
+    public List<Choice> getChoices() {
+        Choice retry = new Choice("Brave the darkness again", null, ChoiceType.RESTART);
+
+        return switch (endType) {
+            case GAME_OVER -> List.of(
+                    retry,
+                    new Choice("Admit defeat", null, ChoiceType.QUIT)
+            );
+            case WIN -> List.of(
+                    retry,
+                    new Choice("Accept freedom", null, ChoiceType.QUIT)
+            );
+        };
     }
 
     @Override
-    public void onEnter(Game game) {
-        super.onEnter(game);
-        game.setGameOver();
+    public void addChoice(Choice choice) {
+        throw new UnsupportedOperationException("Choices for EndNodes are pre-defined");
     }
 }
