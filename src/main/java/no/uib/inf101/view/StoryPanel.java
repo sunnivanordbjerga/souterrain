ac077d2929c00f270e700f13a84005e9a6e87188
sunniva.bjerga <subje9045@uib.no>
2026-05-05 19:46:05 +0200

    implemented basic StoryPanel

--- src/main/java/no/uib/inf101/view/StoryPanel.java
+++ src/main/java/no/uib/inf101/view/StoryPanel.java
@@ -3,10 +3,42 @@ package no.uib.inf101.view;
 import no.uib.inf101.model.game.Choice;
 import no.uib.inf101.model.game.node.Node;
 
+import javax.swing.*;
+import java.awt.*;
+import java.util.List;
+
+
 /**
  * Displays the {@link Node} text, {@link Choice}s and health points.
  */
-public class StoryPanel {
+public class StoryPanel extends JPanel {
+    private final JTextArea textArea;
 
-    //TODO StoryPanel
+    public StoryPanel(UITheme theme) {
+        this.textArea = new JTextArea();
+        this.setBackground(theme.background());
+
+        configureTextArea(theme);
+
+        this.setLayout(new BorderLayout());
+        add(new JScrollPane(textArea), BorderLayout.CENTER);
+    }
+
+    public void update(List<String> log){
+        textArea.setText(String.join("\n", log));
+    }
+
+    private void configureTextArea(UITheme theme) {
+        textArea.setEditable(false);
+        textArea.setLineWrap(true);
+        textArea.setWrapStyleWord(true);
+        textArea.setRows(40);
+        textArea.setColumns(20);
+        textArea.setCaretPosition(textArea.getDocument().getLength());
+
+        textArea.setBackground(theme.background());
+        textArea.setForeground(theme.textColor());
+        textArea.setFont(theme.text());
+        textArea.setFocusable(false);
+    }
 }
