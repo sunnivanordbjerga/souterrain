f460bb898a3f173b937a8d93316032c15dfff2ba
sunniva.bjerga <subje9045@uib.no>
2026-05-05 02:06:40 +0200

    added ChoiceType

--- src/main/java/no/uib/inf101/model/game/narration/Choice.java
+++ src/main/java/no/uib/inf101/model/game/narration/Choice.java
@@ -9,20 +9,36 @@ import java.util.Objects;
  *
  * @param text     the text describing this choice; cannot be blank
  * @param nextNode the resulting node from choosing this choice
+ * @param type     the {@link ChoiceType}, representing system behavior;
+ *                 default {@code PROGRESS_STORY}
  */
-public record Choice(String text, Node nextNode) {
+public record Choice(String text, Node nextNode, ChoiceType type) {
 
     /**
-     * Creates a {@link Choice}.
+     * Creates a {@link Choice} with {@link ChoiceType} {@code PROGRESS_STORY}.
      *
      * @param text     the text describing this choice; cannot be blank
      * @param nextNode the resulting node from choosing this choice
      * @throws NullPointerException     if text or nextNode is null
      * @throws IllegalArgumentException if text is blank
      */
+    public Choice(String text, Node nextNode) {
+        this(text, nextNode, ChoiceType.PROGRESS_STORY);
+    }
+
+    /**
+     * Creates a {@link Choice}.
+     *
+     * @param text     the text describing this choice; cannot be blank
+     * @param nextNode the resulting node from choosing this choice
+     * @param type     the {@link ChoiceType}, defining its system behavior
+     * @throws NullPointerException     if text, nextNode or type is null
+     * @throws IllegalArgumentException if text is blank
+     */
     public Choice {
         Objects.requireNonNull(text, "Text cannot be null");
         Objects.requireNonNull(nextNode, "NextNode cannot be null");
+        Objects.requireNonNull(type, "ChoiceType cannot be null");
         if (text.isBlank()) {
             throw new IllegalArgumentException("Text cannot be blank");
         }
---
+++ src/main/java/no/uib/inf101/model/game/narration/ChoiceType.java
@@ -0,0 +1,10 @@
+package no.uib.inf101.model.game.narration;
+
+/**
+ * High level representation of system event choice outcomes
+ */
+public enum ChoiceType {
+    PROGRESS_STORY,
+    RESTART,
+    QUIT
+}
