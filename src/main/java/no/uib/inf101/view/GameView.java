0b078fc48de0f35df025f2c9ac222c23650c881f
sunniva.bjerga <subje9045@uib.no>
2026-05-05 19:46:57 +0200

    started implementing GameView

--- src/main/java/no/uib/inf101/view/GameView.java
+++ src/main/java/no/uib/inf101/view/GameView.java
@@ -1,15 +1,19 @@
 package no.uib.inf101.view;
 
-import no.uib.inf101.model.game.node.Node;
-
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * Renders the current game state by delegating to
- * the {@link StoryPanel}, {@link ChoicePanel} and {@link ScenePanel}.
+ * the {@link StoryPanel}, {@link ChoicePanel}.
  */
-public class GameView {
-    //TODO GameView
+public class GameView extends JPanel {
+    private final ViewableGame game;
+    private final UITheme theme;
+    private final StatPanel statPanel;
+    private final ChoicePanel choicePanel;
+    private final StoryPanel storyPanel;
+
 
     /**
      * Creates a new {@link GameView}.
@@ -17,5 +21,35 @@ public class GameView {
      * @param game the game to render
      */
     public GameView(ViewableGame game) {
+        this.game = game;
+        this.theme = UITheme.defaultTheme();
+        this.statPanel = new StatPanel(theme);
+        this.choicePanel = new ChoicePanel(theme);
+        this.storyPanel = new StoryPanel(theme);
+
+        this.setLayout(new BorderLayout());
+
+        this.add(statPanel, BorderLayout.PAGE_START);
+        this.add(storyPanel, BorderLayout.CENTER);
+        this.add(choicePanel, BorderLayout.PAGE_END);
+    }
+
+    @Override
+    public void paintComponent(Graphics g) { //
+        super.paintComponent(g);
+        Graphics2D g2 =(Graphics2D) g;
+        //TODO: draw background frame and title, else delete
+    }
+
+    /**
+     * Renders the {@link ViewableGame}'s current state.
+     */
+    public void render() {
+        storyPanel.update(game.getLog());
+        choicePanel.update(game.getCurrentNode().getChoices());
+        statPanel.update(game.getPlayerHp());
+
+        revalidate();
+        repaint();
     }
 }
